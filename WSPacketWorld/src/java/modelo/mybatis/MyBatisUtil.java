ca291106f08f66d8587ec4381c472aca4b143001
IsaidOchoa <isaid3472@gmail.com>
2025-12-15 19:15:38 -0600

    testeo de conexion

--- WSPacketWorld/src/java/modelo/mybatis/MyBatisUtil.java
+++ WSPacketWorld/src/java/modelo/mybatis/MyBatisUtil.java
@@ -17,13 +17,13 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
  */
 public class MyBatisUtil {
     private static final String RESOURCE ="modelo/mybatis/mybatis-config.xml";
-    private static final String ENVIORMENT = "desarrollo";
+    private static final String ENVIROMENT = "desarrollo";
     
     public static SqlSession getSession() {
     SqlSession session = null ;
         try {
             Reader reader = Resources.getResourceAsReader(RESOURCE);
-            SqlSessionFactory sqlMapper = new SqlSessionFactoryBuilder().build(reader);
+            SqlSessionFactory sqlMapper = new SqlSessionFactoryBuilder().build(reader, ENVIROMENT);
             session = sqlMapper.openSession();
         } catch (Exception e) {
         }
